cb388030b67ca341bcdf8721df9ce91b4915e05e
Zoe <hxy9349@gmail.com>
2025-05-05 03:11:19 +0800

    C#楊華萱B11170011-作業四

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿namespace Chap9Test2_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box;
+            box = new Box(5, 5, 5);
+
+            Console.WriteLine("盒子體積：" + box.Volume());
+            Console.WriteLine("盒子面積：" + box.Area());
+        }
+    }
+
+    public class Box
+    {
+        public double Width, Height, Length; //Properties
+        public Box(double width, double height, double length) //Constructors
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Length = length;
+        }
+
+        public double Volume()  //Method Volume()
+        {
+            return Width * Height * Length; 
+        }
+        public double Area()  //Method Area()
+        {
+            double area = Width * Height;
+            return area;
+        }
+    }
+}
